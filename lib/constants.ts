56d721f0213bf4f37381ff151e4b8d46b7945a95
Kirubel <kirubeldesalegn17@gmail.com>
2025-01-29 15:52:46 +0300

    layout

---
+++ lib/constants.ts
@@ -0,0 +1,3 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'Souq'
+export const APP_SLOGAN = process.env.NEXT_PUBLIC_APP_SLOGAN || 'At your doorstep!'
+export const APP_DESCRIPTION = process.env.NEXT_PUBLIC_APP_DESCRIPTION || 'an ecommerce web built with nextjs'
